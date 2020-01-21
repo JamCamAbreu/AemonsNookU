675e9c758419883faf80cb2d0176db12841e55f0
James Cameron Abreu <coolcam222@gmail.com>
2020-01-21 07:50:30 -0500

    Begin Project
    + Tile class
    + World Class
    + Import sprites from previous project

---
+++ AemonsNookU/Assets/Prefabs/World/Tile.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+
+    // Remember: A Tile is not a prefab. It's just a C# class,
+    // that has data and methods. The World class will be responsible
+    // for drawing the tiles using the tilemap system in unity.
+
+    public enum Type 
+    {
+        grass,
+        road,
+        water,
+    }
+
+    /// Properties
+    public int posX { get; set; }
+    public int posY { get; set; }
+    public bool isBuilding { get; set; }
+    public bool isPath { get; set; }
+
+    public Tile TileAbove { get; set; }
+    public Tile TileRight { get; set; }
+    public Tile TileBelow { get; set; }
+    public Tile TileLeft { get; set; }
+
+    private void Awake()
+    {
+        isBuilding = false;
+        isPath = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateTileType(Type t)
+    {
+
+    }
+
+
+
+}
---
+++ AemonsNookU/Assets/Prefabs/World/World.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour
+{
+
+    // TODO:
+    // World class will also be responsible for drawing and maintaining the tiles (tilemap tiles)
+    // This should not be updated every step, but only when needed
+
+
+    #region INTERFACE
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    #endregion
+
+
+    // ------------- Properties ----------------
+    #region PROPERTIES
+    public int WorldWidth;
+    public int WorldHeight;
+    private Tile[][] WorldTiles;
+    #endregion
+
+
+
+
+
+    // --------------- NEIGHBOR -----------------
+    #region NEIGHBORS
+    public void SetNeighbors()
+    {
+    }
+    #endregion
+
+
+
+
+
+    // --------------- NATURE -----------------
+    #region NATURE
+    public void GrowTrees()
+    {
+    }
+    #endregion
+
+    public void GrowStone()
+    {
+    }
+
+    public void GrowFlowers()
+    {
+    }
+
+    public void ClearTiles()
+    {
+    }
+
+
+
+    // --------------- BUILDINGS -----------------
+    #region BUILDINGS
+    #endregion
+
+
+
+
+    // --------------- SYSTEM -----------------
+    #region SYSTEM
+    private void DeleteTiles()
+    {
+    }
+
+    private Tile InitTileAt(int xPos, int yPos)
+    {
+        Tile curTile = new Tile();
+        curTile.transform.position = new Vector3(xPos, yPos);
+        curTile.posX = xPos;
+        curTile.posY = yPos;
+        return curTile;
+    }
+
+    private void GridInit()
+    {
+        for (int h = 0; h < WorldHeight; h++)
+        {
+            WorldTiles[h] = new Tile[WorldWidth]; 
+            for (int w = 0; w < WorldWidth; w++)
+            {
+                Tile curTile = InitTileAt(w, h);
+                curTile.UpdateTileType(Tile.Type.grass);
+                WorldTiles[h][w] = curTile;
+            }
+        }
+    }
+    #endregion
+
+
+
+    // --------------- DEBUG -----------------
+    #region DEBUG
+    public void SetTileDebugText()
+    {
+    }
+
+    #endregion
+
+
+}
